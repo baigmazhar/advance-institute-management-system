cee7c168be625b6caae862cb2223a09ab7f3e3cb
mazhar <mazharbaig07@gmail.com>
2023-06-18 12:50:57 +0530

    initial springboot setup for aim

---
+++ src/main/java/com/aim/advanceinstitutemanagement/AdvanceinstitutemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.aim.advanceinstitutemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceinstitutemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceinstitutemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aim/advanceinstitutemanagement/AdvanceinstitutemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aim.advanceinstitutemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceinstitutemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
